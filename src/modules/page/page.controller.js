b2195f297a76e65bf904f5433a548940016a3488
kasra <kasramajdy81@gmail.com>
2025-01-11 00:16:19 +0330

    dvelope button follow

--- src/modules/page/page.controller.js
+++ src/modules/page/page.controller.js
@@ -1,5 +1,7 @@
 const hasAccessToPage = require("../../utils/hasAccessToPage");
 const FollowModel = require("./../../models/Follow");
+const UserModel = require("../../models/User");
+const { use } = require("./page.routes");
 
 exports.getPage = async (req, res, next) => {
   try {
@@ -20,7 +22,7 @@ exports.getPage = async (req, res, next) => {
       });
     }
     return res.render("page/index", {
-      followed: Boolean(true),
+      followed: Boolean(followed),
       pageID,
     });
   } catch (err) {
@@ -30,7 +32,38 @@ exports.getPage = async (req, res, next) => {
 
 exports.follow = async (req, res, next) => {
   try {
-    res.json({ message: "User Followed Successfully :))" });
+    const user = req.user;
+    const { pageID } = req.params
+
+    const targetOnePage = await UserModel.findOne({ _id: pageID })
+
+    if (!targetOnePage){
+      req.flash("error", "page not found to follow");
+      return  res.redirect(`/pages/${pageID}`);;
+    }
+
+    if (user._id.toString() === pageID){
+      req.flash("error", "you cannot follow your page");
+      return  res.redirect(`/pages/${pageID}`);;
+    }
+
+    const existingFollow = await FollowModel.findOne({
+      follower: user._id,
+      following: pageID
+    })
+
+    if (existingFollow){
+      req.flash("error", "page already followed");
+      return  res.redirect(`/pages/${pageID}`);;
+    }
+
+    await FollowModel.create({
+      follower: user._id,
+      following: pageID
+    })
+
+    req.flash("success", "page followed successfully");
+    return  res.redirect(`/pages/${pageID}`);;
   } catch (err) {
     next(err);
   }
